4271656f833eb914d01f01dbc245797e9a11e509
AntonMitsura <mitsuraanton@mail.ru>
2019-10-24 21:40:23 +0300

    <Домашнее задание lesson5 про книги и библиотеки, методы takeHome и getInfo>

--- src/lesson5/Library.java
+++ src/lesson5/Library.java
@@ -31,14 +31,38 @@ public class Library {
     // Если недоступна - то написать, что недоступна
     // Если выдана, то написать, что уже выдана
     public String getInfo(String title){
-        return "Кника найдена. Название: . Автор: . Доступна в читальном зале/для выдачи на дом. Для выдачи доступна/пока недоступна";
+        for (int i = 0; books[i] != null; i++) {
+            if (books[i].getAuthor().equals(title)){
+                String bookInfo = "Книга найдена. Название: " + books[i].getTitle() + ". Автор: " + books[i].getAuthor() + ". Доступна в читальном зале/для выдачи на дом. Для выдачи доступна/пока недоступна";
+                bookInfo = books[i].getForHome() ? bookInfo + " Для выдачи на дом." : bookInfo + " Доступна в читальном зале.";
+                bookInfo = books[i].getAvailable() ? bookInfo + " Для выдачи доступна." : bookInfo + " Для выдачи пока доступна.";
+                return bookInfo;
+            }
+        }
+        System.out.println("Книга не найдена");
+        return null;
     }
 
 //    Должна быть возможность взять книгу на дом, указав название
 //    Должна быть возможность почитать книгу в читальном зале, указав название.
     // Книги, с указанным названием в библиотеке может не быть
     public Book takeHome(String title){
-        // если книга с указанным
+        for (int i = 0; books[i] != null; i++) {
+            if (books[i].getAuthor().equals(title)){
+                if (books[i].getAvailable() && books[i].getForHome()){
+                    books[i].setAvailable(false);
+                    return books[i];
+                } else if (books[i].getAvailable() && !books[i].getForHome()){
+                    System.out.println("Книгу нельзя взять на дом. Она только для читального зала");
+                    return null;
+                } else {
+                    System.out.println("Книга уже выдана. К выдаче сейчас недоступна.");
+                    return null;
+                }
+            }
+        }
+        System.out.println("Книга не найдена");
+        return null;
     }
 
 
